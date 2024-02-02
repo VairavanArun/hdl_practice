0267d938f31e99a871a2598479cccc5e68ff089c
Vairavan <vairavan.arun13@gmail.com>
2024-02-02 14:31:50 -0600

    Added testbench for testing 16 bit PPA

---
+++ lab2_sign_mag/ppa_16bit_tb.sv
@@ -0,0 +1,23 @@
+module testbench();
+    logic [15:0] A,B;
+    logic Cin;
+    logic [15:0] Sum;
+    logic Cout;
+
+    assign Cin = 0;
+
+    ppa_16bit DUT(A, B, Cin, Sum, Cout);
+
+    initial begin
+        A = 16'h0; B = 16'h0; #60;
+        A = 16'h0321; B = 16'h0132; #60;
+        A = 16'hFFFF; B = 16'hFFFF; #60;
+        A = 16'h0001; B = 16'hFFFF; #60;
+        A = 16'hAAAA; B = 16'h5555; #60;
+        A = 16'h5555; B = 16'hAAAA; #60;
+        A = 16'h8000; B = 16'h7FFF; #60;
+        A = 16'h8000; B = 16'h0001; #60;
+        A = 16'h7FFF; B = 16'h7000; #60;
+    end
+
+endmodule 
