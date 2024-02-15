dc976bfeef390e982852ba8c17c9f6e23950f732
Vairavan <vairavan.arun13@gmail.com>
2024-02-15 14:11:58 -0600

    Updated ALU design

--- lab4_mips_alu/mips_alu.sv
+++ lab4_mips_alu/mips_alu.sv
@@ -3,6 +3,16 @@
  * ALU can perform ADD, SUB, AND, OR, SLT
  */
 
+module and_32bit(input logic [31:0] A,B,
+                 output logic [31:0] Y);
+    assign Y = A & B;
+endmodule
+
+module or_32bit(input logic [31:0] A,B,
+                 output logic [31:0] Y);
+    assign Y = A | B;
+endmodule
+
 module mips_alu(input logic [31:0]  A, B,
                 input logic [2:0]   F,
                 output logic [31:0] Y,
@@ -14,15 +24,25 @@ module mips_alu(input logic [31:0]  A, B,
 
     assign negated_B = F[2] ? (~B):B;
 
+    //instantiate and module
+    and_32bit and_circ(A, B, D0);
+    //instantiate or module
+    or_32bit or_circ(A, B, D1);
     //instantiate adder
     ppa_32bit adder(A, B, F[2], D2, Cout);
 
-    assign D0 = A & negated_B;
-    assign D1 = A | negated_B;
-    assign D3 = {31'b0, D2[31]};
+    assign Zero = (Y == 32'b0) ? 1'b1 : 1'b0;
 
     //get the output of the ALU
-    assign Y = F[1] ? (F[0] ? D3 : D2) : (F[0] ? D1 : D0);
+    always_comb
+        begin : ALU_output_block
+            case (F[1:0])
+                2'b00: Y = D0; 
+                2'b01: Y = D1;
+                2'b10: Y = D2;
+                2'b11: Y = D3;
+            endcase
+        end
 
 
 endmodule 
