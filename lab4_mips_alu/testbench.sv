239f05974c16e64dc61592ee415752cdefc43054
Tanvi Susarla <tanvimurty@gmail.com>
2024-02-17 17:25:54 -0600

    Update testbench.sv
    testbench for ALU module with overflow func

--- lab4_mips_alu/testbench.sv
+++ lab4_mips_alu/testbench.sv
@@ -3,16 +3,17 @@ module testbench();
     logic [31:0] A, B, Y, Y_expected;
     logic [2:0] F;
     logic Z, Z_expected;
+    logic OF,OF_expected;
     logic [31:0] vectornum, errors;
-    logic [104:0] test_vectors[100000:0];
+    logic [108:0] test_vectors[100000:0];
 
     //instantiate the ALU
-    mips_alu dut(A, B, F, Y, Z);
+    mips_alu dut(A, B, F, Y, Z,OF);
 
     //read the test vector file
     initial
         begin
-            $readmemh("D:/class_work/semester2/ece469/hdl_practice/lab4_mips_alu/alu.tv", test_vectors);
+            $readmemh("C:/Users/tanvi/OneDrive/Documents/UIC spring24sem2/ece469HDL/Lab3/testvectorModuleC.tv.txt", test_vectors);
             vectornum = 0;
             errors = 0;
             reset = 1; #30; reset = 0;
@@ -28,24 +29,25 @@ module testbench();
     always @(posedge clk)
         begin
             #1;
-            F = test_vectors[vectornum][102:100];
-            A = test_vectors[vectornum][99:68];
-            B = test_vectors[vectornum][67:36];
-            Y_expected = test_vectors[vectornum][35:4];
-            Z_expected = test_vectors[vectornum][0];
+            F = test_vectors[vectornum][106:104];
+            A = test_vectors[vectornum][103:72];
+            B = test_vectors[vectornum][71:40];
+            Y_expected = test_vectors[vectornum][39:8];
+            Z_expected = test_vectors[vectornum][4];
+	    OF_expected = test_vectors[vectornum][0];
         end
     
     always @(negedge clk)
         begin
             if (~reset) begin
 
-                if ((Y != Y_expected) || (Z != Z_expected)) begin
-                    $display("\t\tError: \t\tinput = %h-%h-%h | exp_output = %h-%h | actual_output = %h-%h | Testvector - %h", F, A, B,Y_expected, Z_expected, Y, Z, vectornum);
+                if ((Y != Y_expected) || (Z != Z_expected) || OF !=OF_expected) begin
+                    $display("\t\tError: \t\tinput = %h-%h-%h | exp_output = %h-%h-%h | actual_output = %h-%h-%h | Testvector - %h", F, A, B,Y_expected, Z_expected,OF_expected, Y, Z, OF, vectornum);
                     errors = errors + 1;
                 end
 
-                if ((Y == Y_expected) || (Z == Z_expected)) begin
-                    $display("\t\tPassed: \t\tinput = %h-%h-%h | exp_output = %h-%h | actual_output = %h-%h | Testvector - %h", F, A, B,Y_expected, Z_expected, Y, Z, vectornum);
+                if ((Y == Y_expected) || (Z == Z_expected) || OF!=OF_expected) begin
+                    $display("\t\tPassed: \t\tinput = %h-%h-%h | exp_output = %h-%h-%h | actual_output = %h-%h-%h | Testvector - %h", F, A, B,Y_expected, Z_expected,OF_expected, Y, Z, OF, vectornum);
                 end
 
                 if (A === 32'bx) begin
