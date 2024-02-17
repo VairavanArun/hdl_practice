9c90c7324c9f2dc740fd1e62ff72fdd603ecbc7b
Tanvi Susarla <tanvimurty@gmail.com>
2024-02-17 17:24:11 -0600

    Update mips_alu.sv
    Updated mips_alu module with overflow and slt functionality

--- lab4_mips_alu/mips_alu.sv
+++ lab4_mips_alu/mips_alu.sv
@@ -32,29 +32,29 @@ endmodule
 module mips_alu(input logic [31:0]  A, B,
                 input logic [2:0]   F,
                 output logic [31:0] Y,
-                output logic        Zero);
+                output logic        Zero,OF);
 
     logic [31:0] D0, D1, D2, D3;
     logic [31:0] negated_B;
     logic [31:0] pattern_match_out;
     logic Cout;
-
+    logic SLT;
     assign negated_B = F[2] ? (~B):B;
 
-    assign D3 = F[2] ? {31'b0, D2[31]} : pattern_match_out;
+    assign D3 = F[2] ? {31'b0, SLT} : pattern_match_out; 
 
     //instantitate pattern match module
     pattern_match pattern_match_circ(A, B, pattern_match_out);
     //instantiate and module
-    and_32bit and_circ(A, B, D0);
+    and_32bit and_circ(A, negated_B, D0);
     //instantiate or module
-    or_32bit or_circ(A, B, D1);
+    or_32bit or_circ(A, negated_B, D1);
     //instantiate adder
     ppa_32bit adder(A, negated_B, F[2], D2, Cout);
 
     //get the output of the ALU
     mux4to1 multiplexer(D0, D1, D2, D3, F[1:0], Y);
-
+    overflow of(A[31],B[31],F,D2[31],OF,SLT);
     assign Zero = (Y == 32'b0) ? 1'b1 : 1'b0;
     
 endmodule 
